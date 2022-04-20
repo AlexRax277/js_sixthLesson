9fa1dd8e0f4d9db02eeafc7bdded71be1f84d06a
Александр Раковчен <italianhorse20@gmail.com>
2022-04-20 14:02:48 +0300

    fourth iteration

--- task2/task.js
+++ task2/task.js
@@ -1,11 +1,14 @@
 const menu = document.querySelector('.menu')
 const menuLinks = Array.from(menu.getElementsByClassName('menu__link'))
 let click_foo = event => {
-    if (event.target.closest('.menu__item').querySelector('.menu_sub').classList.contains('menu_active')) {
-        event.target.closest('.menu__item').querySelector('.menu_sub').classList.remove('menu_active')
-    } else if (event.target.classList.contains('menu__link')) {
-        event.target.closest('.menu__item').querySelector('.menu_sub').classList.add('menu_active')
+    if (event.target.closest('.menu__item').querySelector('.menu_sub')) {
+        if (event.target.closest('.menu__item').querySelector('.menu_sub').classList.contains('menu_active')) {
+            event.target.closest('.menu__item').querySelector('.menu_sub').classList.remove('menu_active')
+        } else if (event.target.classList.contains('menu__link')) {
+            event.target.closest('.menu__item').querySelector('.menu_sub').classList.add('menu_active')
+        }
     }
+    
 }
 
 
